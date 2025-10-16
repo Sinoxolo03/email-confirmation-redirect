ddea867fbe3c8d1e5dc0cf43e5ee754b6c7ee31b
Sinoxolo <nzolodaka@gmail.com>
2025-10-17 01:39:22 +0200

    .

--- supabase-client.js
+++ supabase-client.js
@@ -1,7 +1,7 @@
-// supabase-client.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
+// supabase/supabaseClient.ts
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl ='https://wptwecrgylbjcsrndeob.supabase.co'.trim();
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwdHdlY3JneWxiamNzcm5kZW9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1NzAxMDksImV4cCI6MjA2NDE0NjEwOX0.Zu-Ma5mB4oCGJJBaiRYYUIKJWX7gOdUcdetCG_P0rHY'.trim();
 
-const SUPABASE_URL = 'https://wptwecrgylbjcsrndeob.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwdHdlY3JneWxiamNzcm5kZW9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1NzAxMDksImV4cCI6MjA2NDE0NjEwOX0.Zu-Ma5mB4oCGJJBaiRYYUIKJWX7gOdUcdetCG_P0rHY'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
