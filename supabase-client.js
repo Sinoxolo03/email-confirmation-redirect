252c374486cc10d8b6b311ac3138bc939c429608
Sinoxolo <nzolodaka@gmail.com>
2025-10-17 17:07:22 +0200

    Added a new supabaseClient.js file

--- supabase-client.js
+++
@@ -1,7 +0,0 @@
-// supabase/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js';
-const supabaseUrl ='https://wptwecrgylbjcsrndeob.supabase.co'.trim();
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwdHdlY3JneWxiamNzcm5kZW9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1NzAxMDksImV4cCI6MjA2NDE0NjEwOX0.Zu-Ma5mB4oCGJJBaiRYYUIKJWX7gOdUcdetCG_P0rHY'.trim();
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
---
+++ supabaseClient.js
@@ -0,0 +1,8 @@
+// supabaseClient.js
+// @ts-nocheck
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  "https://wptwecrgylbjcsrndeob.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwdHdlY3JneWxiamNzcm5kZW9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1NzAxMDksImV4cCI6MjA2NDE0NjEwOX0.Zu-Ma5mB4oCGJJBaiRYYUIKJWX7gOdUcdetCG_P0rHY"
+);
